3fc736bc9ae2483244e7ff930d85356f8f4a60b1
himanshu-45naik <himanshugit45@gmail.com>
2025-01-22 09:10:15 +0530

    Feature Engineering

--- steps/feature_engineering_step.py
+++ steps/feature_engineering_step.py
@@ -4,7 +4,7 @@ from src.feature_engineering import FeatureEngineer,OneHotEncoding,MinMaxScaling
 
 
 @step 
-def feature_engineering(df:pd.DataFrame,strategy:str)->pd.DataFrame:
+def feature_engineering(df:pd.DataFrame,strategy:str,features:list)->pd.DataFrame:
     """Performs feature engineering on the data.
 
     Args:
@@ -14,16 +14,17 @@ def feature_engineering(df:pd.DataFrame,strategy:str)->pd.DataFrame:
         pd.DataFrame: The transformed data frame.
     """
     if strategy == "log":
-        feature_engineer = FeatureEngineer(LogTransformation)
+        feature_engineer = FeatureEngineer(LogTransformation(features))
     elif strategy == "standard":
-        feature_engineer = FeatureEngineer(StandardScaling)
+        feature_engineer = FeatureEngineer(StandardScaling(features))
     elif strategy == "min-max":
-        feature_engineer = FeatureEngineer(MinMaxScaling)
+        feature_engineer = FeatureEngineer(MinMaxScaling(features))
     elif strategy == "onehotencoding":
-        feature_engineer = FeatureEngineer(OneHotEncoding)
+        feature_engineer = FeatureEngineer(OneHotEncoding(features))
     else:
         raise ValueError(f"Unsupported feature engineering strategy:{strategy}")
 
-    transformed_data = feature_engineer.apply_transformation
+    
+    transformed_data = feature_engineer.apply_transformation(df)
     
     
